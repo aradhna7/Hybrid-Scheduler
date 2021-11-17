0a929863a14a1ddb1c5e6603ca07aac0ffbf8118
aradhna7 <aradhna.7sharma@gmail.com>
2021-11-17 14:18:22 +0530

    routes

--- server.js
+++ server.js
@@ -20,11 +20,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    let url = path.join(__dirname, '../client/build', 'index.html');
-    if (!url.startsWith('/app/'))
-      // since we're on local windows
-      url = url.substring(1);
-    res.sendFile(url);
+    res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
   });
 }
 
