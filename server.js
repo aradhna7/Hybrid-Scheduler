20c16eb0e364e7be112bb91bc03c4df77c79ba40
aradhna7 <aradhna.7sharma@gmail.com>
2021-11-17 12:44:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/user', require('./routes/api/user'));
+app.use('/api/admin', require('./routes/api/admin'));
+app.use('/api/products', require('./routes/api/product'));
+
+//SERVE STATIC ASSETS IN PRODUCTION
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(
+  port,
+  console.log(`server is running in ${process.env.NODE_ENV} on port ${port}`)
+);
