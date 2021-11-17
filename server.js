74262f4df66ab7fdc73e0596f0db762afd26fb4e
aradhna7 <aradhna.7sharma@gmail.com>
2021-11-17 21:06:59 +0530

    deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
+var path = require('path');
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/slot', require('./routes/slotRoutes'));
